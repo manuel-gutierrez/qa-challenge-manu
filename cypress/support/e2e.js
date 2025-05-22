46e28b84f368c9aaf8655fa034f7c8d9b4b9e71c
Manuel Gutierrez <menrique.gutierrez@gmail.com>
2025-05-22 11:03:14 -0500

    remove:  huge examples

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,7 +2,6 @@
 import '../support/commands/login-commands';
 import '../support/commands/register-commands';
 // import '@percy/cypress';
-// import '@hugetools/test-helper'
 import '@shelex/cypress-allure-plugin';
 const  compareSnapshotCommand = require('cypress-image-diff-js/command');
 compareSnapshotCommand();
